e5f7e76dec7203a6aa9c9ed6da5ca41649bfbd39
Miguel A. Ibarra-Arellano <miguel.ibarra@uni-heidelberg.de>
2024-09-23 16:38:44 +0200

    Add training with and without Cross-Validation.

--- src/model/main.py
+++ src/model/main.py
@@ -7,7 +7,6 @@ from dataset import CINDataset
 from models import micronuclAI
 from augmentations import get_transforms
 from pytorch_lightning.loggers import CSVLogger
-from utils import evaluate_binary_model, evaluate_multiclass_model, plot_confusion_matrix
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import argparse
@@ -38,20 +37,20 @@ def get_args():
     # Training options
     training = parser.add_argument_group(title="Training")
     training.add_argument("-m", "--model", dest="model", action="store", default="efficientnet_b0",
-                            help="Model to use for training. [Default = efficientnet_b0]")
+                          help="Model to use for training. [Default = efficientnet_b0]")
     training.add_argument("-s", "--size", dest="size", action="store", default=(256, 256), type=int, nargs="+",
-                            help="Size of images for training. [Default = (256, 256)]")
+                          help="Size of images for training. [Default = (256, 256)]")
     training.add_argument("-b", "--batch_size", dest="batch_size", action="store", default=32, type=int,
-                            help="Batch size for training. [Default = 32]")
+                          help="Batch size for training. [Default = 32]")
     training.add_argument("-w", "--num_workers", dest="num_workers", action="store", default=4, type=int,
                           help="Number of workers for data loading. [Default = 4]")
     training.add_argument("-p" "--precision", dest="precision", action="store", default="32",
                           choices=["16-mixed", "bf16-mixed", "16-true", "bf16-true", "32", "64"],
                           help="Precision for training. [Default = 32]")
     training.add_argument("-sc", "--single_channel", dest="single_channel", action="store_true",
-                            help="Use single channel images. [Default = False]")
+                          help="Use single channel images. [Default = False]")
     training.add_argument("-k", "--kfold", dest="kfold", action="store", default=5, type=int,
-                            help="Number of folds for cross validation. [Default = 5]")
+                          help="Number of folds for cross validation. [Default = 5]")
 
     # Script options
     options = parser.add_argument_group(title="Script options")
@@ -59,7 +58,6 @@ def get_args():
                          choices=["debug", "info"],
                          help="Set the logging level. [Default = info]")
     options.add_argument("--version", action="version", version="micronuclAI 1.0.0")
-
 
     # Parse arguments
     args = parser.parse_args()
@@ -91,6 +89,123 @@ def save_model(model, output_path, suffix="") -> None:
     torch.jit.save(script, MODEL_FILE)
 
 
+def cv_training(args, data_train, data_test, hparams) -> None:
+    """
+    Perform k-fold cross-validation and train the model on each fold.
+
+    :param args: Arguments containing model, size, batch_size precision, kfold and output path.
+    :param data_train: Training data
+    :param data_test: Test data
+    :param hparams: Hyperparameters for the model.
+    :return: None
+    """
+
+    # Stratified K-Fold split
+    skf = StratifiedKFold(n_splits=args.kfold, shuffle=True, random_state=42)
+
+    # Iterate through each fold
+    for k, (train_indices, val_indices) in enumerate(skf.split(data_train.df["image"], data_train.df["label"])):
+        # Set logger
+        csv_logger = CSVLogger(
+            args.out,
+            name=f"{args.model}_s{args.size[0]}_bs{args.batch_size}_p{args.precision}",
+            version=f"log"
+        )
+
+        # Get subsets for training, validation, and test
+        train_set = torch.utils.data.Subset(data_train, train_indices)
+        val_set = torch.utils.data.Subset(data_train, val_indices)
+        datasets = (train_set, val_set, data_test)
+
+        # Print dataset sizes
+        print(f"Training data   = {len(train_set)}")
+        print(f"Validation data = {len(val_set)}")
+        print(f"Test data       = {len(data_test)}")
+
+        # Initialize model with hyperparameters, datasets, and model name
+        model = micronuclAI(hparams, datasets, args.model)
+
+        # Trainer setup
+        trainer = pl.Trainer(
+            precision=args.precision,
+            accelerator="auto",
+            max_epochs=300,
+            callbacks=[EarlyStopping(monitor="val_loss", mode="min")],
+            logger=csv_logger
+        )
+
+        # Train the model
+        trainer.fit(model)
+
+        # Save model after training for the current fold
+        save_model(model, args.out, suffix=f"_{k}")
+
+        # Validation: get validation scores
+        model.get_val_pred_scores(output_path=args.out, suffix=f"_{k}")
+
+        # Test: get test scores
+        model.get_test_pred_scores(output_path=args.out, suffix=f"_{k}")
+
+
+def no_cv_training(args, data_train, data_test, hparams) -> None:
+    """
+    Train a model with no cross-validation on all the data.
+    
+    :param args: Arguments containing model, size, batch_size precision, kfold and output path.
+    :param data_train: Training data
+    :param data_test: Test data
+    :param hparams: Hyperparameters for the model.
+    :return: None
+    """
+    # Set logger
+    csv_logger = CSVLogger(
+        args.out,
+        name=f"{args.model}_s{args.size[0]}_bs{args.batch_size}_p{args.precision}",
+        version=f"log"
+    )
+
+    # Split data into train and test (uses 1/10 of data for testing)
+    train_indices, val_indices = train_test_split(data_train.df.index,
+                                                  test_size=0.05,
+                                                  stratify=data_train.df["label"],
+                                                  random_state=42)
+
+    train_set = torch.utils.data.Subset(data_train, train_indices)
+    val_set = torch.utils.data.Subset(data_train, val_indices)
+
+    # Get subsets for training, validation, and test
+    datasets = (train_set, val_set, data_test)
+
+    # Print final ammount of training, validation and test data
+    print(f"Training data   = {len(train_set)}")
+    print(f"Validation data = {len(val_set)}")
+    print(f"Test data       = {len(data_test)}")
+
+    # Initialize model with hyperparameters, datasets, and model name
+    model = micronuclAI(hparams, datasets, args.model)
+
+    # Trainer setup
+    trainer = pl.Trainer(
+        precision=args.precision,
+        accelerator="auto",
+        max_epochs=300,
+        callbacks=[EarlyStopping(monitor="val_loss", mode="min")],
+        logger=csv_logger
+    )
+
+    # Train the model
+    trainer.fit(model)
+
+    # Save model after training for the current fold
+    save_model(model, args.out)
+
+    # Validation: get validation scores
+    model.get_val_pred_scores(output_path=args.out)
+
+    # Test: get test scores
+    model.get_test_pred_scores(output_path=args.out)
+
+
 def main():
     torch.set_float32_matmul_precision('high')
     # Read arguments from command line
@@ -103,17 +218,17 @@ def main():
     lg.info(f"Batch size = {args.batch_size}")
     lg.info(f"Image size = {args.size}")
 
-
     # Load transformations for train and validation
     lg.info("Loading transformations")
     transform = {
         "train": get_transforms(resize=args.size, single_channel=args.single_channel, training=True),
-        "test":   get_transforms(resize=args.size, single_channel=args.single_channel, training=False)
+        "test": get_transforms(resize=args.size, single_channel=args.single_channel, training=False)
     }
 
-    # Create two dataset objects with different transformations (to not have augmentations in validation and test set later)
+    # Create two dataset objects with different transformations
+    # (to not have augmentations in validation and test set later)
     data_train = CINDataset(csv_path=args.labels, images_folder=args.images, transform=transform["train"])
-    data_test  = CINDataset(csv_path=args.test, images_folder=args.images, transform=transform["test"])
+    data_test = CINDataset(csv_path=args.test, images_folder=args.images, transform=transform["test"])
     lg.info(f"Dataset contains  = {len(data_train)} images.")
     lg.info(f"Data distribution = \n{data_train.df['label'].value_counts()} images.")
 
@@ -124,57 +239,13 @@ def main():
         "workers": args.num_workers,
     }
 
-    # Cross validation k-fold
-    skf = StratifiedKFold(n_splits=args.kfold, shuffle=True, random_state=42)
-    for k, (train_indices, val_indices) in enumerate(skf.split(data_train.df["image"],
-                                                               data_train.df["label"])):
-
-        # Set loggers
-        csv_logger = CSVLogger(args.out,
-                               name=f"{args.model}_s{args.size[0]}_bs{args.batch_size}_p{args.precision}",
-                               version=f"log")
-
-        # Get subsets of data for train/validation and test
-        train_set = torch.utils.data.Subset(data_train, train_indices)
-        val_set = torch.utils.data.Subset(data_train, val_indices)
-        #test_set = torch.utils.data.Subset(data_test, data_test.df.index) # Use all indexes for the test dataset
-        datasets = (train_set, val_set, data_test)
-
-        # Print final ammount of training, validation and test data
-        print(f"Training data   = {len(train_set)}")
-        print(f"Validation data = {len(val_set)}")
-        print(f"Test data       = {len(data_test)}")
-
-
-
-        # Set model, pass parameters, datasets and model name to use
-        model = micronuclAI(hparams, datasets, args.model)
-
-        # Training model
-        trainer = pl.Trainer(
-            precision=args.precision,
-            accelerator="auto",
-            max_epochs=300,
-            #log_every_n_steps=1,
-            callbacks=[EarlyStopping(monitor="val_loss", mode="min")],
-            logger=csv_logger
-        )
-        trainer.fit(model)
-
-        ########################################
-        # Save model
-        save_model(model, args.out, suffix=f"_{k}")
-
-        ########################################
-        # VALIDATION
-        # Get validation scores
-        model.get_val_pred_scores(output_path=args.out, suffix=f"_{k}")
-
-        ########################################
-        # TEST
-        # Get test scores
-        model.get_test_pred_scores(output_path=args.out, suffix=f"_{k}")
-
+    # Call training with k-folds
+    lg.info(f"Training model ")
+    if args.kfold != 0:
+        cv_training(args, data_train, data_test, hparams)
+    else:
+        no_cv_training(args, data_train, data_test, hparams)
+    lg.info("Done!")
 
 if __name__ == "__main__":
     main()
